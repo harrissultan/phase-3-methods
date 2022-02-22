f71fd5aebea65484afa0aba2cb365ac4e7923285
Harris Sultan <harris_sultan@outlook.com>
2022-02-21 23:10:27 -0500

    commit tree 0e2caf17aefa8b0cff9e23171d43501f717e252b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+def greet(name)
+    puts "Hello, #{name}!"
+end
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+def add(num1, num2)
+    return num1 + num2
+end
+def halve(num)
+    return nil unless num.class == Integer
+  
+    num / 2
+end
+
+greet_programmer
+greet("Naureen")
+greet("Jimmy")
+greet_with_default("Naureen")
+greet_with_default
+# sum =add(5,8) 
+# hj=halve(9)
